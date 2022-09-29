297ac979e705a5da9fc08de5e26e833540f71cab
Luneldevwb <nematt007@gmail.com>
2022-09-29 08:02:56 +0300

    Added new scripts for hamburger menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,53 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const nav = document.querySelector('.nav'),
+    navItem = document.querySelectorAll('.nav_item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        nav.classList.toggle('nav_active');
+    });
+
+    navItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            nav.classList.toggle('nav_active');
+        })
+    })
+}); window.addEventListener('DOMContentLoaded', () => {
+    const nav = document.querySelector('.nav'),
+    navItem = document.querySelectorAll('.nav_item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        nav.classList.toggle('nav_active');
+    });
+
+    navItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            nav.classList.toggle('nav_active');
+        })
+    })
+}); window.addEventListener('DOMContentLoaded', () => {
+    const nav = document.querySelector('.nav'),
+    navItem = document.querySelectorAll('.nav_item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        nav.classList.toggle('nav_active');
+    });
+
+    navItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            nav.classList.toggle('nav_active');
+        })
+    })
+})
+
 var slideIndex = 1;
 showSlides(slideIndex);
 function plusSlides(n) {
